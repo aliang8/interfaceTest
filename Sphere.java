38ffa58ea8f327b6b015372c5582b78c33ada468
Anthony Liang <aliang8@stuy.edu>
2015-12-10 17:23:45 -0500

    Made Prism, Sphere, Cylinder, and Volume interface

---
+++ Sphere.java
@@ -0,0 +1,14 @@
+public class Sphere extends Circle implements Volume{
+
+    public Sphere(double radius, String name){
+	super(radius,name);
+    }
+
+    public double getVolume() {
+	return (4*Math.PI*Math.pow(getRadius(),3))/3;
+    }
+
+    public String toString(){
+	return "Sphere "+getName()+" with radius of "+getRadius();
+    }
+}
